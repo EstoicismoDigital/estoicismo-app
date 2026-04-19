4628b4a2388a2d4378043faa241c424fe7c67ba2
gruponitidoic-web <gruponitidoic@gmail.com>
2026-04-19 13:42:28 -0400

    chore: use as const on fontWeights object for consistency

--- packages/ui/src/tokens/typography.ts
+++ packages/ui/src/tokens/typography.ts
@@ -26,8 +26,8 @@ export const lineHeights = {
 } as const;
 
 export const fontWeights = {
-  regular: "400" as const,
-  medium: "500" as const,
-  semibold: "600" as const,
-  bold: "700" as const,
-};
+  regular: "400",
+  medium: "500",
+  semibold: "600",
+  bold: "700",
+} as const;
